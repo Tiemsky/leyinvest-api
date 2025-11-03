bb8b20cd42eba66e76872d18f5e2634db56d2d61
Tiemsky <42318635+Tiemsky@users.noreply.github.com>
2025-11-03 13:49:33 +0000

    Fxing BocIndicateur decimal seeder

--- database/seeders/BocIndicatorSeeder.php
+++ database/seeders/BocIndicatorSeeder.php
@@ -17,11 +17,11 @@ class BocIndicatorSeeder extends Seeder
         BocIndicator::truncate();
         DB::table('boc_indicators')->insert([
             'date_rapport' => '2025-10-31',
-            'taux_rendement_moyen' => 7,53,
+            'taux_rendement_moyen' => 7.53,
             'key' => 'boc_ley20251024',
-            'per_moyen' => 14,20,
-            'taux_rentabilite_moyen' => 8,08,
-            'prime_risque_marche' => 1,29,
+            'per_moyen' => 14.20,
+            'taux_rentabilite_moyen' => 8.08,
+            'prime_risque_marche' => 1.29,
             'source_pdf' => 'https://www.brvm.org/sites/default/files/boc_20251031_2.pdf',
             'created_at' => '2025-01-01 06:19:38',
             'updated_at' => now(),
