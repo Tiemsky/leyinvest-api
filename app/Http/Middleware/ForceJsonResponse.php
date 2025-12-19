06ec865bdd35b5e60f473601ceb75312704b9b74
Tiemsky <42318635+Tiemsky@users.noreply.github.com>
2025-12-19 09:59:47 +0000

    Fix:Rate Limit

--- app/Http/Middleware/ForceJsonResponse.php
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -10,7 +10,6 @@ class ForceJsonResponse
     public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
-
         // Force JSON pour toutes les requêtes API
         if ($request->is('api/*') && !$response->headers->has('Content-Type')) {
             $response->headers->set('Content-Type', 'application/json');
